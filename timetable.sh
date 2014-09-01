72b3156df63759a9eafdd6a1c0ee4e82615695c0
igorjan94 <igorjan94@mail.ru>
2014-09-01 23:18:21 +0000

    added ok timetable

--- timetable.sh
+++ timetable.sh
@@ -1,14 +1,26 @@
-#usage: ./timetable.sh destination_directory source_directory
-a=$2
-b=$1
+#usage: ./timetable.sh [destination_directory] [source_directory]
 
 #setting default directories
-if [ $# -le 1 ]
-	then a="/home/igorjan/206round"
-fi
+a="/home/igorjan/206round"
+b="/home/igorjan/documents/timetable"
+flag=0;
+
+while getopts "s:d:0" opt; do
+  case $opt in
+    \?) exit 1;;
+    s) a=$OPTARG;;
+    d) b=$OPTARG;;
+    0) flag=1;;
+  esac
+done
+
+shift $(($OPTIND - 1))
 if [ $# -eq 0 ]
-	then b="/home/igorjan/documents/timetable"
+	then echo "usage: ./timetable.sh [-d <destination_directory>] [-s <source_directory>] \"from1 to1 ... fromN toN\""
+	exit
 fi
+
+arg=$1
 
 #create source_directory, clean temporary files
 mkdir -p $b
@@ -19,19 +31,18 @@ c="$a/timetableparser.py"
 a="$a/loadtimetable.py"
 
 #downloading timetable
-python3 $a $b "Лигово" "Тайцы"\
- "Тайцы" "Лигово"\
- "Лигово" "Санкт-Петербург-Балт."\
- "Санкт-Петербург-Балт." "Лигово"\
- "Санкт-Петербург-Финл." "Зеленогорск"\
- "Зеленогорск" "Санкт-Петербург-Финл."
+python3 $a $b $arg
 
 #parsing every downloaded file
 for file in $b/*.txt
 do
-	python3 $c $file $file.out 1
+    if [ $flag -eq 0 ]
+        then python3 $c $file $file.out
+    else
+        python3 $c $file $file.out 1
+    fi
 done 
 
 #final renaming and deleting temp files
 rm -f $b/*.txt
-rename '.out' '' $b/*
+rename '.txt.out' '.out' $b/*
