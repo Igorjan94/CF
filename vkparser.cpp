32fb878b3abd53c637d025c710dec6a761bcb46f
igorjan94 <igorjan94@mail.ru>
2014-09-18 00:10:35 +0000

    alpha version vkparser

--- vkparser.cpp
+++ vkparser.cpp
@@ -41,10 +41,18 @@ void writeln(){printf("\n");}void writeln2(){printf("\n");}void readln(){}
 
 ///----------------------------------------------------------------------------------------------------------------------------
 
-int n, m, k;
-vi a;
 string buffer;
-
+CURL *curl;
+CURLcode res;
+Json::Value root, items;   
+Json::Reader reader;
+string key = "\
+5fd6eba49abd5a1f94127f33803605a4660221181c8f36cdf84864e21d868982635d40ed62e95ff3407cf\
+";
+string url = "https://api.vk.com/method/wall.get?owner_id=-29253653&count=5&v=5.24&access_token=" + key;
+string getMessage = "https://api.vk.com/method/wall.getComments?owner_id=-29253653&count=10&v=5.24&sort=desc&access_token=" + key + "&post_id=";
+string get = "https://oauth.vk.com/authorize?client_id=4552027&redirect_uri=https://oauth.vk.com/blank.html&scope=12&response_type=token";
+string getName = "https://api.vk.com/method/users.get?v=5.24&user_ids=";
 size_t curl_write( void *ptr, size_t size, size_t nmemb, void *stream)
 {           
     buffer.append((char*)ptr, size*nmemb);
@@ -60,36 +68,43 @@ void notify(string s, string t)
 
 string itoa(int i)
 {
-    string s = "1";
-    s[0] = i + 48;
+    string s;
+    while (i >= 10)
+        s.pb(i % 10 + 48),
+        i /= 10;
+    s.pb(i + 48);
+    reverse(whole(s));
     return s;
+}
+
+Json::Value parse(string url)
+{
+    root.clear();
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, url.data()); 
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    if (res != CURLE_OK)
+        writeln("Error in curl");
+    if (!reader.parse(buffer, root))
+        writeln("Failed to parse configuration\n", reader.getFormatedErrorMessages());
+    buffer.clear();
+    return root;
 }
 
 void run()
 {
     Notify::init("init");
-    string url = "https://api.vk.com/method/wall.get?owner_id=-29253653&count=5&v=5.24&access_token=83899af71a7ca0503e3faa5ec56eb60aefd5f44371a3f59dff22debbe817bc169dac953bbeb345a925ea6";
-    string get = "https://oauth.vk.com/authorize?client_id=4552027&redirect_uri=https://oauth.vk.com/blank.html&scope=12&response_type=token";
-    CURL *curl;
-    CURLcode res;
     curl_global_init(CURL_GLOBAL_DEFAULT);
-    int counter = 0;
     vector< pair<string, int> > pairs, temp;
     writeln("all inits done");//------------------------------------------
-    while (++counter < 10)
+    int counter = 0;
+    while (counter >= 0)
     {
-        curl = curl_easy_init();
-        curl_easy_setopt(curl, CURLOPT_URL, url.data()); 
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
-        res = curl_easy_perform(curl);
-        writeln("got data from server");//---------------------------
-        curl_easy_cleanup(curl);
-        Json::Value root;   
-        Json::Reader reader;
-        bool parsingSuccessful = reader.parse( buffer, root );
-        if ( !parsingSuccessful )
-            std::cout  << "Failed to parse configuration\n" << reader.getFormatedErrorMessages();
-        const Json::Value items = root["response"]["items"];
+        writeln(counter++);
+        items.clear();
+        items = parse(url)["response"]["items"];
         fori(items.size())
         {
             Json::Value text = items[i]["text"];
@@ -97,11 +112,10 @@ void run()
             ((counter == 1) ? pairs : temp).pb({text.asString(), comments.asInt()});
         }
         if (counter == 1)
+        {
             fori(pairs.size())
-            {
                 notify("Message", pairs[i].first + " (" + itoa(pairs[i].second) + ")");
-                writeln(pairs[i]);//-------------------------------
-            }
+        }
         else
         {
             if (pairs[0].first != temp[0].first)
@@ -113,13 +127,19 @@ void run()
             else
             {
                 fori(pairs.size())
-                    if (pairs[i].second != temp[i].second)
-                        notify("New comment", temp[i].first),
+                    if (pairs[i].second < temp[i].second)
+                    {
+                        auto x = parse(getMessage + itoa(items[i]["id"].asInt()))["response"]["items"];
+                        ROF(j, temp[i].second - pairs[i].second - 1, 0)
+                        {
+                            auto y = parse(getName + itoa(x[j]["from_id"].asInt()))["response"][(unsigned int) 0];
+                            notify("New comment to " + temp[i].first, y["first_name"].asString() + " " + y["last_name"].asString() + ": " + x[j]["text"].asString());
+                        }
                         pairs[i].second = temp[i].second;
+                    }
             }
         }
         temp.clear();
-        buffer.clear();
         sleep(AUTO_REFRESH);
     }
     curl_global_cleanup();
