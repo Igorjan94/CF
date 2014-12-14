70e516238e1bbd9c61ba998f5d1a29992d9181d5
igorjan94 <igorjan94@mail.ru>
2014-12-14 23:37:05 +0300

    timetable -> json

--- timetable/loadtimetable.py
+++ timetable/loadtimetable.py
@@ -1,59 +1,82 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
 from collections import defaultdict
 import re
 import sys
 import os
 import getopt
+import requests
+
+def defineDays(s):
+    s = s.replace('ежедневно', '')
+    s = s.replace('по выходным', 'СВ')
+    s = s.replace('по рабочим', 'КСВ')
+    s = s.replace('по воскресеньям', 'В')
+    s = s.replace('по субботам', 'С')
+    s = s.replace('по пятницам', 'П')
+    s = s.replace('кроме пятн. и субб.', 'КПС')
+    s = s.replace('кроме пятниц и вых.', 'КПСВ')
+    s = s.replace('кроме воскресений', 'КВ')
+    s = s.replace('по пятницам и выходным', 'ПСВ')
+    s = re.sub(r'.*отменен.*', '', s)
+    return s
+
+def defineTime(s):
+    if flag == 1:
+        s = re.sub(r'\t\t00:[34]\d:00', ' б/о', s)
+        s = re.sub(r'.*?\d\d:[1..9]\d:00.*?', '', s)
+        s = re.sub(r'\d\d:\d\d:\d\d', '', s)
+    else:
+        s = re.sub(r'00:(\d\d):00', r'\1 м', s)
+        s = re.sub(r'0(\d):(\d\d):00', r'\1 ч \2 м', s)
+        s = re.sub(r'(\d\d):(\d\d):00', r'\1 ч \2 м', s)
+    return s
+
+def removeRepeatingStations(s):
+    if flag == 0:
+        return s
+    s = re.sub(r'Санкт-Петербург-.*?(\s|$)', r'\1', s)
+    s = s.replace('Зеленогорск', '')
+    s = s.replace('Лигово', '')
+    s = s.replace('Калище', '')
+    s = s.replace('Лебяжье', '')
+    s = s.replace('Ораниенбаум-1', '')
+    s = s.replace('Новый Петергоф', '')
+    s = s.replace('Гатчина Балтийская', 'Г')
+    s = s.replace('Гатчина Варшавская', 'Г')
+    return s
+
 
 def parse(url):
-    html_doc = urlopen(url).read()
-    soup = BeautifulSoup(html_doc)
+    x = requests.get(url).json()
+    filename = directory + reve[x["dep-st"]][0] + '—' + reve[x["arr-st"]][0]
+    if not (x["dat"] is None):
+        filename += ", " + x["dat"]
+    filename = re.sub(r'(Санкт-Петербург)-.*?\.', r'\1', filename)
+    x = x["tra-list"]
+    sys.stdout = open(filename + ".out", 'w')
+    for y in x:
+        s = y["tra"]["sch"] + "\t\t" + reve[y["tra"]["dep"]["st"]][0] + "\t\t" + reve[y["tra"]["arr"]["st"]][0] + "\t\t" + y["tra"]["tr-tim"]
+        s = defineDays(s)
+        s = defineTime(s)
+        s = removeRepeatingStations(s)
+        if s != "":
+            print(y["tra"]["dep"]["tim"] + "\t\t" + s)
 
-    for entry in soup:
-        lines = re.split('\n', str(entry))
-        temp = 0
-        for line in lines:
-            if line.find('пересадками') != -1:
-                temp = 1
-        if temp == 0:
-            for line in lines:
-                if line.find('<title>') != -1:
-                    line = re.sub(r'^.*?<title>Расписание электричек (.*?)\..*', r'\1', line)
-                    line = re.sub(r'\s', '', line)
-                    sys.stdout = open(directory + '/' + line + '.my', 'a')
-                    continue
-                line = re.sub(r'<a href=".*?">(.*?)</a>', r'\1', line)
-                line = re.sub(r'^\s+(.*?)\s+</td>', r'\1', line)
-                line = re.sub(r'$nbsp;', r' ', line)
-                line = re.sub(r'<span class="miniNbsp">', r'', line)
-                line = re.sub(r'</span>', r'', line)
-                line = re.sub(r'^\s+(\d+.*?(м|ч).*?)', r'\1', line)
-                line = re.sub(r'^[#a-zA-Z-<_\s$(.{}/].*', r' ', line)
-                line = re.sub(r'^\s+', r'', line)
-                if not line.startswith(' ') and not line.startswith('	') and not line == '':
-                    if temp == 0:
-                        cur = '		' + line
-                    else:
-                        cur = cur + '		' + line
-                    temp = temp + 1
-                if temp == 6:
-                    temp = 0
-                    print(cur)
 st = os.path.dirname(os.path.abspath(__file__)) + '/stations.out'
 file = open(st, 'r')
 hash = defaultdict(list)
+reve = defaultdict(list)
 for string in file:
-    l = string.split('\t')
+    l = string[:-1].split('\t')
     hash[l[0]].append(l[1])
+    reve[l[1]].append(l[0])
+
 directory = sys.argv[1]
-today = sys.argv[2]
-args = sys.argv[3:]
+today     = sys.argv[2]
+flag      = sys.argv[3]
+args      = sys.argv[4:]
+if directory[-1] != os.sep:
+    directory += os.sep
 for i in range(0, len(args), 2):
     for fro in hash[args[i]]:
         for to in hash[args[i + 1]]:
-            url = 'http://www.tutu.ru/spb/rasp.php?st1=' + fro + '&st2=' + to + '&print=yes'
-            url += today
-
-            url = url.replace('\n', '')
-            parse(url)
+            parse('http://www.tutu.ru/spb/rasp.php?st1=' + fro + '&st2=' + to + '&json' + today)
--- timetable/timetable.sh
+++ timetable/timetable.sh
@@ -1,14 +1,20 @@
-#usage: ./timetable.sh [destination_directory] [source_directory]
+#usage: ./timetable.sh [-t] [-f <DD.MM.YYYY>] [-0] [-d <destination_directory>] [-s <source_directory>] \"from1 to1 ... fromN toN\""
 
-#setting default directories
+#setting default parametres
 a="/home/igorjan/206round/timetable"
 b="/home/igorjan/documents/timetable"
 flag=0;
 today="";
 
+function usage {
+   echo "usage: ./timetable.sh [-t] [-f <DD.MM.YYYY>] [-0] [-d <destination_directory>] [-s <source_directory>] \"from1 to1 ... fromN toN\"" 
+   exit
+}
+
+#parsing arguments
 while getopts "0ts:d:f:" opt; do
   case $opt in
-    \?) exit 1;;
+    \?) usage;;
     s) a=$OPTARG;;
     d) b=$OPTARG;;
     0) flag=1;;
@@ -19,33 +25,16 @@ done
 
 shift $(($OPTIND - 1))
 if [ $# -eq 0 ]
-	then echo "usage: ./timetable.sh [-t] [-f <DD.MM.YYYY>] [-0] [-d <destination_directory>] [-s <source_directory>] \"from1 to1 ... fromN toN\""
-	exit
+	then usage
 fi
 
 arg=$1
 
 #create source_directory, clean temporary files
 mkdir -p $b
-rm -f $b/*.my
 
 #setting script pathes
-c="$a/timetableparser.py"
 a="$a/loadtimetable.py"
 
 #downloading timetable
-python3 $a $b "$today" $arg
-
-#parsing every downloaded file
-for file in $b/*.my
-do
-    if [ $flag -eq 0 ]
-        then python3 $c $file $file.out
-    else
-        python3 $c $file $file.out 1
-    fi
-done 
-
-#final renaming and deleting temp files
-rm -f $b/*.my
-rename '.my.out' '.out' $b/*
+python3 $a $b "$today" $flag $arg
