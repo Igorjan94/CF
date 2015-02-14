946eaa4162fedd58a383c07c7a88cb9fa8b98e69
igorjan94 <igorjan94@mail.ru>
2015-02-15 01:13:06 +0300

    514

---
+++ writeln.h
@@ -0,0 +1,30 @@
+//Igorjan94, template version from 23 January 2015
+#include <bits/stdc++.h>
+using namespace std;
+
+#define tthti  template<typename Head, typename... Tail> inline
+#define ttt12i template<typename T1, typename T2> inline
+#define ttti   template<typename T> inline
+
+inline void writeln2(){cout<<"\n";}
+inline void writeln() {cout<<"\n";}
+inline void readln()  {}
+ttti void read(T&);
+ttti void priws(T);
+ttti void print(T);
+
+void err(vector<string>::iterator it){++it;}
+tthti void readln (Head& head,Tail&... tail){read(head); readln  (tail...);}
+tthti void writeln2(Head head, Tail... tail){print(head);writeln2(tail...);}
+tthti void writeln (Head head, Tail... tail){priws(head);writeln2(tail...);}
+ttti  void writeln_range(T f,T s){if(f!=s){priws(*f);for(auto i=++f;i!=s;++i)print(*i);}writeln();}
+tthti void err(vector<string>::iterator it,Head head,Tail...tail){writeln((*it).substr((*it)[0]==' '),"=",head);err(++it, tail...);}
+vector<string>split(const string&s,char c){vector<string>v;stringstream ss(s);string x;while(getline(ss,x,c))v.push_back(x);return move(v);}
+ttti   ostream&operator<<(ostream&os,vector<T>&a);
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>&a){return os<<a.first<<" "<<a.second;}
+ttti   ostream&operator<<(ostream&os,vector<T>&a){if(a.size())os<<a[0];else os<<"\n";for(int i=1;i<a.size();++i)os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   istream&operator>>(istream&is,vector<T>&a){if(a.size()==0){int n;is>>n;a.resize(n);}for(int i=0;i<a.size();++i)is>>a[i];return is;}
+ttti void print(T a){cout<<" "<<a;}
+ttti void priws(T a){cout<<a;}
+ttti void read(T& a){cin>>a;}
