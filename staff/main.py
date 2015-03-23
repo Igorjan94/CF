38627bb2d088445fe5e5a089750a2678642ce817
igorjan94 <igorjan94@mail.ru>
2015-03-23 22:50:53 +0300

    main.py

---
+++ staff/main.py
@@ -0,0 +1,35 @@
+import sys
+
+def main():
+    writeln_range(sorted(read_int_array()))
+    writeln(1, 2, 3, 4, 5)
+
+
+
+
+
+
+def read():
+    return sys.stdin.readline().rstrip('\n')
+
+def read_array(sep=None, maxsplit=-1):
+    return read().split(sep, maxsplit)
+
+def read_int():
+    return int(read())
+
+def read_int_array(sep=None, maxsplit=-1):
+    return list(map(int, read_array(sep, maxsplit)))
+
+def writeln(*args, **kwargs):
+    sep = kwargs.get('sep', ' ')
+    end = kwargs.get('end', '\n')
+    sys.stdout.write(sep.join(str(a) for a in args) + end)
+
+def writeln_range(array, **kwargs):
+    sep = kwargs.get('sep', ' ')
+    end = kwargs.get('end', '\n')
+    sys.stdout.write(sep.join(str(a) for a in array) + end)
+
+if __name__ == '__main__':
+    main()
