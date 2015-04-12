812065fc73247c7abc153185cc3b274a83519a43
igorjan94 <igorjan94@mail.ru>
2015-04-12 15:05:24 +0300

    gcjD

--- 2015/gcjQual/D.cpp
+++ 2015/gcjQual/D.cpp
@@ -11,6 +11,6 @@ int main()
         std::cin >> x >> n >> m;
         if (n > m)
             std::swap(n, m);
-        std::cout << "Case #" << ++test << ": " << ans[((n * m) % x != 0) || (n < x && m < x) || (x > 6) || (n <= x - 2)] << "\n";
+        std::cout << "Case #" << ++test << ": " << ans[((n * m) % x != 0) || (n < x && m < x) || (x > 6) || x == 5 && (n <= 2 || n == 3 && m == 5) || x == 6 && n <= 3 || x == 3 && n <= 1 || x == 4 && n <= 2] << "\n";
     }
 }
