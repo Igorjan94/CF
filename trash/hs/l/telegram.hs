647b3beed32d4b9c1155fe08c6578d726c97371d
igorjan94 <igorjan94@mail.ru>
2016-01-25 06:53:14 +0300

    hs

--- trash/hs/l/telegram.hs
+++ trash/hs/l/telegram.hs
@@ -11,15 +11,20 @@ import Control.Lens
 
 import Data.Maybe
 
-import Data.Vector (mapM)
+import Data.Vector (mapM, Vector)
 import Data.Text (Text, pack)
+import Text.Printf
 
 import Network
 import Exchange
 
+token :: String
 token = "130250270:AAFeKP-ZbsdRxc4g5QtkbFYmrF5POgOaurw"
+
+api :: String
 api = "https://api.telegram.org/bot" 
 
+getUpdates :: IO (Data.Vector.Vector Value)
 getUpdates = do
     response <- jsonByURL $ api ++ token ++ "/getUpdates"
     let Just status = response ^? key "ok" . _Bool
@@ -32,49 +37,48 @@ sendMessage chatId (Just text) = do
     response <- jsonByURL $ api ++ token ++ "/sendMessage?chat_id=" ++ show chatId ++ "&text=" ++ text
     let Just bool = response ^? key "ok" . _Bool
     return bool
-sendMessage chatId Nothing = return True
+sendMessage _ Nothing = return True
 
 kostyl :: Text -> String
 kostyl x = init $ tail $ show x
 
 getWithUsd :: Value -> String -> Maybe Double
-getWithUsd conversion "USD" = Just 1
-getWithUsd conversion to = conversion ^? key (pack to) . _Double
+getWithUsd _ "USD" = Just 1
+getWithUsd conversion value = conversion ^? key (pack value) . _Double
 
 convert :: [String] -> IO (Maybe String)
-convert (q : from : _ : to : xs) = do
+convert (q : src : _ : dest : _) = do
     conversion <- getConversion
-    let x = getWithUsd conversion from
-    let y = getWithUsd conversion to
+    let x = getWithUsd conversion src
+    let y = getWithUsd conversion dest
     let quantity = read q :: Double
     case x of 
         Nothing -> return Nothing
         Just base -> case y of
             Nothing -> return Nothing
-            Just value -> return $ Just (q ++ " " ++ from ++ " equals to " ++ show (quantity * value / base) ++ " " ++ to)
+            Just value -> return $ Just (q ++ " " ++ src ++ " equals dest " ++ printf "%.2f" (quantity * value / base) ++ " " ++ dest)
 convert _ = return Nothing
 
-markAsRead :: (Integer, Bool) -> IO()
-markAsRead (updateId, True) = print $ "markAsRead " ++ show updateId
-markAsRead _ = print "fuuuu"
+markAsRead :: (Integer, Bool) -> IO ()
+markAsRead (updateId, True) = print $ "markedAsRead " ++ show updateId
+markAsRead (updateId, False) = print $ "message is not delivered: " ++ show updateId
 
 proceedMessage :: Value -> IO (Integer, Bool)
 proceedMessage update = do
-    let Just id = update ^? key "update_id" . _Integer
+    let Just updateId = update ^? key "update_id" . _Integer
     let Just message = update ^? key "message"
-    let name = message ^? key "from" . key "first_name" . _String
+    {-let name = message ^? key "from" . key "first_name" . _String-}
     let Just chatId = message ^? key "chat" . key "id" . _Integer
     let Just text = message ^? key "text" . _String
     let strings = words $ kostyl text
     answer <- convert strings
     response <- sendMessage chatId answer
-    return (id, response)
+    return (updateId, response)
 
 telegram :: IO()
 telegram = forever $ do
     updates <- getUpdates
-    {-print updates-}
     proceededMessages <- Data.Vector.mapM proceedMessage updates
-    proceededMessages <- Data.Vector.mapM markAsRead proceededMessages
+    _ <- Data.Vector.mapM markAsRead proceededMessages
     print proceededMessages
     threadDelay 5000000
