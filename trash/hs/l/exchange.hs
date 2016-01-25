c1de3b8a267723ba9397c3cb174b0908d3194e4c
igorjan94 <igorjan94@mail.ru>
2016-01-25 06:16:01 +0300

    hs

--- trash/hs/l/exchange.hs
+++ trash/hs/l/exchange.hs
@@ -5,7 +5,19 @@ module Exchange where
 import Control.Monad
 import Control.Concurrent
 
+import Data.Aeson
+import Data.Aeson.Lens
+import Control.Lens
+
+import Data.Maybe
+
 import Network
+
+getConversion :: IO Value
+getConversion = do
+    response <- jsonByURL "http://api.fixer.io/latest?base=USD"
+    let Just conversion = response ^? key "rates"
+    return conversion
 
 exchange :: IO()
 exchange = forever $ do
--- trash/hs/l/telegram.hs
+++ trash/hs/l/telegram.hs
@@ -12,9 +12,10 @@ import Control.Lens
 import Data.Maybe
 
 import Data.Vector (mapM)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 
 import Network
+import Exchange
 
 token = "130250270:AAFeKP-ZbsdRxc4g5QtkbFYmrF5POgOaurw"
 api = "https://api.telegram.org/bot" 
@@ -31,14 +32,27 @@ sendMessage chatId (Just text) = do
     response <- jsonByURL $ api ++ token ++ "/sendMessage?chat_id=" ++ show chatId ++ "&text=" ++ text
     let Just bool = response ^? key "ok" . _Bool
     return bool
-sendMessage chatId Nothing     = return True
+sendMessage chatId Nothing = return True
 
 kostyl :: Text -> String
 kostyl x = init $ tail $ show x
 
-convert :: [String] -> Maybe String
-convert (quantity : from : _ : to : xs) = Just (quantity ++ " " ++ from ++ " equals to " ++ quantity ++ " " ++ to)
-convert _ = Nothing
+getWithUsd :: Value -> String -> Maybe Double
+getWithUsd conversion "USD" = Just 1
+getWithUsd conversion to = conversion ^? key (pack to) . _Double
+
+convert :: [String] -> IO (Maybe String)
+convert (q : from : _ : to : xs) = do
+    conversion <- getConversion
+    let x = getWithUsd conversion from
+    let y = getWithUsd conversion to
+    let quantity = read q :: Double
+    case x of 
+        Nothing -> return Nothing
+        Just base -> case y of
+            Nothing -> return Nothing
+            Just value -> return $ Just (q ++ " " ++ from ++ " equals to " ++ show (quantity * value / base) ++ " " ++ to)
+convert _ = return Nothing
 
 markAsRead :: (Integer, Bool) -> IO()
 markAsRead (updateId, True) = print $ "markAsRead " ++ show updateId
@@ -52,16 +66,15 @@ proceedMessage update = do
     let Just chatId = message ^? key "chat" . key "id" . _Integer
     let Just text = message ^? key "text" . _String
     let strings = words $ kostyl text
-    let answer = convert strings
+    answer <- convert strings
     response <- sendMessage chatId answer
     return (id, response)
 
 telegram :: IO()
 telegram = forever $ do
     updates <- getUpdates
-    print updates
+    {-print updates-}
     proceededMessages <- Data.Vector.mapM proceedMessage updates
-    print proceededMessages
     proceededMessages <- Data.Vector.mapM markAsRead proceededMessages
     print proceededMessages
     threadDelay 5000000
