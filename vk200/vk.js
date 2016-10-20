da78e47758231bb534d8670b6f8dff3da900974f
igorjan94 <igorjan94@mail.ru>
2016-10-20 21:15:19 +0300

    Revert "vk200"
    This reverts commit a3226f5f88fe31ea890e6b3c65b81aa35a53e8b7.

--- vk200/vk.js
+++
@@ -1,7 +0,0 @@
-'use strict';
- 
-const vk = new (require('vk-io'));
-
-//vk.setToken('00c1e2527b78f9839ef3f7e7931afa190c25c3be6b5503207ccca5c40678d8cab814150d061e20cb0e05a')
-
-console.log(vk.api);
