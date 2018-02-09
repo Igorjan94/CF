05173228448ed33cf80c6cae737b301cbd8153eb
igorjan94 <igorjan94@mail.ru>
2018-02-09 21:59:31 +0300

    main -> c++17

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-// Igorjan94, template version from 19 March 2015 (#define -> typedef, readln(vector) patched, version from 27 March 2017)
+// Igorjan94, template version from 13 October 2017. C++17 version {{{
 #include <bits/stdc++.h>
 
 using namespace std;
+
+#define FILENAME "input"
 
 #define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
 #define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
@@ -16,52 +18,45 @@ typedef  pair<int, int>   pii;
 typedef  valarray<int>    va;
 typedef   vector<int>     vi;
 typedef    long long      ll;
-#define      pb      push_back
-#define   whole(a)   a.begin(), a.end()
-#define    next      next__
-#define    prev      prev__
-#define   count      count__
-#define  argmax(a)   (max_element(whole(a)) - (a).begin())
-#define  argmin(a)   (min_element(whole(a)) - (a).begin())
 
-#define  ints(a...)  int a; readln(a)
-#define  lls(a...)   ll a; readln(a)
-#define wr(args...)  err(split(#args,',').begin(),args)
+#define    pb    push_back
+#define whole(a) a.begin(), a.end()
+#define   next   next__
+#define   prev   prev__
+#define  count   count__
+
+#define ints(a...)  int a; readln(a)
+#define wr(args...) err(#args, args)
+
+#define ttt12i template<class T1, class T2> inline
+#define  tthti template<class H, class...T> inline
+#define  ttta  template<class... Args> inline
+#define  ttti  template<class T> inline
                     
-#define  FILENAME    "input"
-#define     INF      1000000007
+const int MOD = 1000000007;
+const int INTMAX = numeric_limits<int>::max();
+const ll LLMAX = numeric_limits<ll>::max();
                     
-#define    tthti     template<typename Head, typename... Tail> inline
-#define   ttt12i     template<typename T1, typename T2> inline
-#define    ttti      template<typename T> inline
+void writeln(){cout<<"\n";}ttti void print(T a);ttti void priws(T a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H h,T...t){priws(h);(print(t),...);writeln();}
+ttti void writeln_range(T f,T s){if(f!=s)for(auto i=f;i!=s;++i)writeln(*i);}
+vector<string>split(string&s,string d){vector<string> v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
+ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
 
-inline void writeln2(){cout<<"\n";}
-inline void writeln() {cout<<"\n";}
-inline void readln()  {}
-ttti void read(T&);
-ttti void priws(T);
-ttti void print(T);
-
-void err(vector<string>::iterator it){++it;}
-tthti void readln (Head& head,Tail&... tail){read(head); readln  (tail...);}
-tthti void writeln2(Head head, Tail... tail){print(head);writeln2(tail...);}
-tthti void writeln (Head head, Tail... tail){priws(head);writeln2(tail...);}
-ttti  void writeln_range(T f,T s){if(f!=s)for(auto i=f;i!=s;++i)writeln(*i);}
-tthti void err(vector<string>::iterator it,Head head,Tail...tail){writeln((*it).substr((*it)[0]==' '),"=",head);err(++it, tail...);}
-vector<string>split(const string&s,char c){vector<string>v;stringstream ss(s);string x;while(getline(ss,x,c))v.pb(x);return v;}
-
-///-------------------------------------------------------------------------------------------------------------------------------------
 //Igorjan
+//}}}
+///-------------------------------------------------------------------------------------------------------------------------------------
 
 void run()
 {
     ints(n);
-    va a(n);
+    vector<int> a(n);
     readln(a);
-    sort(begin(a), end(a));
-    writeln(a);    
+    sort(whole(a));
+    writeln(a);
 }
 
+//{{{
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -77,37 +72,15 @@ int main()
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #define a _a
 #define n _n
 ttti   ostream&operator<<(ostream&os,vector<T>&a);
 ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
 ttt12i ostream&operator<<(ostream&os,pair<T1,T2>&a){return os<<a.first<<" "<<a.second;}
-ttti   ostream&operator<<(ostream&os,vector<T>&a){if(a.size())os<<a[0];else os<<"\n";for(int i=1;i<a.size();++i)os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
-ttti   istream&operator>>(istream&is,vector<T>&a){for(int i=0;i<a.size();++i)is>>a[i];return is;}
-ttti   ostream&operator<<(ostream&os,valarray<T>&a){if(a.size())os<<a[0];else os<<"\n";for(int i=1;i<a.size();++i)os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
-ttti   istream&operator>>(istream&is,valarray<T>&a){for(int i=0;i<a.size();++i)is>>a[i];return is;}
+ttti   ostream&operator<<(ostream&os,vector<T>&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   ostream&operator<<(ostream&os,valarray<T>&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti   istream&operator>>(istream&is,valarray<T>&a){fori(a.size())is>>a[i];return is;}
 ttti void print(T a){cout<<" "<<a;}
 ttti void priws(T a){cout<<a;}
-ttti void read(T& a){cin>>a;}
+ttti void read(T& a){cin>>a;} //}}}
