311e621006adbf313f1ae6dad154787df579924d
igorjan94 <igorjan94@mail.ru>
2018-03-19 01:33:12 +0300

    trimVideo: fix if video doesn't have audio

--- scripts/trimVideo.py
+++ scripts/trimVideo.py
@@ -32,7 +32,7 @@ parser.add_argument('-s', '--since',    dest = 'since',    type = str,   default
 parser.add_argument('-t', '--till',     dest = 'till',     type = str                   )
 parser.add_argument('-d', '--duration', dest = 'duration', type = str                   )
 parser.add_argument('-o', '--output',   dest = 'output',   type = str                   )
-parser.add_argument('-v', '--verbose',  dest = 'verbose'                                )
+parser.add_argument('-v', '--verbose',  dest = 'verbose',  action = 'store_true'        )
 parser.add_argument('url',                                 type = isUrl                 )
 
 args = parser.parse_args()
@@ -41,7 +41,10 @@ time = getTime(args)
 FFMPEG = 'ffmpeg -i "{0}" {1} -acodec aac -b:a 192k -avoid_negative_ts make_zero "{2}"'
 FFMPEGAUDIO = 'ffmpeg -i "{0}" {1} -i "{2}" {1} -acodec aac -b:a 192k -avoid_negative_ts make_zero -map 0:v:0 -map 1:a:0 "{3}"'
 
-if args.verbose == None:
+def trimDigits(x):
+    return int(re.split('^(\d+)', x)[1])
+
+if not args.verbose:
     FFMPEG += ' -v fatal -stats'
     FFMPEGAUDIO += ' -v fatal -stats'
 
@@ -54,13 +57,19 @@ else:
     yt = YouTube(args.url)
     download_file_path = '{0}.mp4'.format(yt.title)
     if args.output != None: download_file_path = args.output
-    video = yt.streams.filter(only_audio = False).order_by('resolution').desc().all()[0]
+
+    videos, audios = {}, {}
+    for stream in yt.streams.all():
+        if stream.resolution != None:
+            videos[trimDigits(stream.resolution)] = stream
+        if stream.abr != None:
+            audios[trimDigits(stream.abr)] = stream
+    video = videos[max(videos)]
     if not video.audio_codec:
-        audio = streams.filter(only_audio = True).all()[0]
+        audio = audios[max(audios)]
         process_call_str = FFMPEGAUDIO.format(video.url, time, audio.url, download_file_path)
     else:
         process_call_str = FFMPEG.format(video.url, time, download_file_path)
-
 
 status = subprocess.check_call(process_call_str, shell = True)
 
