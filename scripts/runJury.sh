7037dd861b0b364bbd80e0d1260024508bbec4d3
igorjan94 <igorjan94@mail.ru>
2018-04-17 23:45:31 +0300

    tests

--- scripts/runJury.sh
+++ scripts/runJury.sh
@@ -1,8 +1,14 @@
 exe=$1
+dir=$2
+
+if [ ! "$dir" ]; then
+    dir="."
+fi
+
 
 for i in $(seq 1 1000)
 do
-    if [ ! -f "./$i" ]; then break; fi
-    $exe < $i > $i.a
+    if [ ! -f "$dir/$i" ]; then break; fi
+    $exe < $dir/$i > $dir/$i.a
     echo $i
 done
--- scripts/runSolution.sh
+++ scripts/runSolution.sh
@@ -1,12 +1,17 @@
 exe=$1
 checker=$2
+dir=$3
+
+if [ ! "$dir" ]; then
+    dir="."
+fi
 
 for i in $(seq 1 1000)
 do
-    if [ ! -f "./$i" ]; then break; fi
-    $exe < $i > $i.out
-    $checker $i $i.out $i.a
+    if [ ! -f "$dir/$i" ]; then break; fi
+    $exe < $dir/$i > $dir/$i.out
+    $checker $dir/$i $dir/$i.out $dir/$i.a
     ec=$?
-    rm -rf $i.out
+    rm -rf $dir/$i.out
     if [ $ec -eq 1 ]; then break; fi
 done
