5f6c2a98b818f386a3099a9c239dd047817a1c99
Igor Kolobov <igorjan94@mail.ru>
2018-08-07 23:48:55 +0300

    &&, whole

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Igorjan94, template version from 13 October 2017. C++17 version {{{
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 07 August 2018 (&&, whole) {{{
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -20,7 +20,7 @@ typedef   vector<int>     vi;
 typedef    long long      ll;
 
 #define    pb    push_back
-#define whole(a) a.begin(), a.end()
+#define whole(a) begin(a), end(a)
 #define   next   next__
 #define   prev   prev__
 #define  count   count__
@@ -32,25 +32,24 @@ typedef    long long      ll;
 #define  tthti template<class H, class...T> inline
 #define  ttta  template<class... Args> inline
 #define  ttti  template<class T> inline
-                    
+
 const int MOD = 1000000007;
 const int INTMAX = numeric_limits<int>::max();
 const ll LLMAX = numeric_limits<ll>::max();
-                    
-void writeln(){cout<<"\n";}ttti void print(T a);ttti void priws(T a);ttti void read(T& a);
-ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H h,T...t){priws(h);(print(t),...);writeln();}
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
 ttti void writeln_range(T f,T s){if(f!=s)for(auto i=f;i!=s;++i)writeln(*i);}
 vector<string>split(string&s,string d){vector<string> v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
 ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
 
 //Igorjan
 //}}}
-///-------------------------------------------------------------------------------------------------------------------------------------
 
 void run()
 {
     ints(n);
-    vector<int> a(n);
+    vi a(n);
     readln(a);
     sort(whole(a));
     writeln(a);
@@ -81,6 +80,6 @@ ttti   ostream&operator<<(ostream&os,vector<T>&a){if(a.size())os<<a[0];else os<<
 ttti   ostream&operator<<(ostream&os,valarray<T>&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
 ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
 ttti   istream&operator>>(istream&is,valarray<T>&a){fori(a.size())is>>a[i];return is;}
-ttti void print(T a){cout<<" "<<a;}
-ttti void priws(T a){cout<<a;}
+ttti void print(T&& a){cout<<" "<<a;}
+ttti void priws(T&& a){cout<<a;}
 ttti void read(T& a){cin>>a;} //}}}
