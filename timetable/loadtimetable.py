9a468279334a88b4a1540a5aeab1758ffe7b714c
igorjan94 <igorjan94@mail.ru>
2018-08-20 19:32:23 +0300

    timetable beautyfull!

--- timetable/loadtimetable.py
+++ timetable/loadtimetable.py
@@ -6,57 +6,25 @@ import getopt
 import requests
 import subprocess
 
-def defineDays(s):
-    s = s.replace('по пятницам и выходным', 'ПСВ')
-    s = s.replace('по пятницам и субботам', 'ПС')
-    s = s.replace('ежедневно', '')
-    s = s.replace('по выходным', 'СВ')
-    s = s.replace('по рабочим', 'КСВ')
-    s = s.replace('по воскресеньям', 'В')
-    s = s.replace('по субботам', 'С')
-    s = s.replace('по пятницам', 'П')
-    s = s.replace('кроме пятн. и субб.', 'КПС')
-    s = s.replace('кроме пятниц и вых.', 'КПСВ')
-    s = s.replace('кроме четвергов и вых.', 'КЧСВ')
-    s = s.replace('кроме суббот', 'КС')
-    s = s.replace('кроме воскресений', 'КВ')
-    return s
-
 def fastRepl(r):
-    time = int(r.group(1))
-    return ' б/о' if time < minutes else r.group(0)
+    return 'б/о' if int(r.group(1)) < minutes else r.group(0)
+
+dayReplacers = [ ('по пятницам и выходным', 'ПСВ'), ('по пятницам и субботам', 'ПС'), ('ежедневно', ''), ('по выходным', 'СВ'), ('по рабочим', 'КСВ'), ('по воскресеньям', 'В'), ('по субботам', 'С'), ('по пятницам', 'П'), ('кроме пятн\. и субб\.', 'КПС'), ('кроме пятниц и вых\.', 'КПСВ'), ('кроме четвергов и вых\.', 'КЧСВ'), ('кроме суббот', 'КС'), ('кроме воскресений', 'КВ') ]
+placeReplacers = [ ('Лигово', ''), ('Калище', ''), ('Лебяжье', ''), ('Ораниенбаум-1', ''), ('Новый Петергоф', ''), ('Гатчина Балтийская', 'Г'), ('Гатчина Варшавская', 'Г'), ('Зеленогорск', 'Зел'), ('Кирилловское', 'Кир'), ('Каннельярви', 'Кан'), ('Рощино', 'Рощ'), ('Выборг', 'Выб'), ('Гаврилово', 'Гав'), ('Советский', 'Сов'), (r'Санкт-Петербург-.*?(\s|$)', r'\1') ]
+
+freeReplacers = [ (r'00:(\d\d):\d\d', fastRepl), (r'\d\d:[1..9]\d:00', ''), (r'\d\d:\d\d:\d\d', '') ]
+timeReplacers = [ (r'00:(\d\d):00', r'\1 м'), (r'0(\d):00:00', r'\1 ч'), (r'0(\d):(\d\d):00', r'\1 ч \2 м'), (r'(\d\d):(\d\d):00', r'\1 ч \2 м') ]
+
+def replacer(s, repl):
+    for what, how in repl:
+        s = re.sub(what, how, s)
+    return s
 
 def defineTime(s):
-    if flag == 1:
-        s = re.sub(r'00:(\d\d):\d\d', fastRepl, s)
-        s = re.sub(r'\d\d:[1..9]\d:00', '', s)
-        s = re.sub(r'\d\d:\d\d:\d\d', '', s)
-    else:
-        s = re.sub(r'00:(\d\d):00', r' \1 м', s)
-        s = re.sub(r'0(\d):(\d\d):00', r' \1 ч \2 м', s)
-        s = re.sub(r'(\d\d):(\d\d):00', r' \1 ч \2 м', s)
-    return s
+    return replacer(s, timeReplacers if flag == 0 else freeReplacers)
 
 def removeRepeatingStations(s):
-    if flag == 0:
-        return s
-    s = re.sub(r'Санкт-Петербург-.*?(\s|$)', r'\1', s)
-    s = s.replace('Лигово', '')
-    s = s.replace('Калище', '')
-    s = s.replace('Лебяжье', '')
-    s = s.replace('Ораниенбаум-1', '')
-    s = s.replace('Новый Петергоф', '')
-    s = s.replace('Гатчина Балтийская', 'Г')
-    s = s.replace('Гатчина Варшавская', 'Г')
-    s = s.replace('Зеленогорск', 'Зел')
-    s = s.replace('Кирилловское', 'Кир')
-    s = s.replace('Каннельярви', 'Кан')
-    s = s.replace('Рощино', 'Рощ')
-    s = s.replace('Выборг', 'Выб')
-    s = s.replace('Гаврилово', 'Гав')
-    s = s.replace('Советский', 'Сов')
-    return s
-
+    return s if flag == 0 else replacer(s, placeReplacers)
 
 def parse(directory, url):
     try:
@@ -66,30 +34,42 @@ def parse(directory, url):
         return
     if 'error' in x:
         return
+
     filename = directory + reve[x['dep-st']][0] + '—' + reve[x['arr-st']][0]
     if not (x['dat'] is None):
         filename += ', ' + x['dat']
     filename = re.sub(r'(Санкт-Петербург)-.*?\.', r'\1', filename)
-    x = x['tra-list']
     sys.stdout = open(filename + '.out', 'w')
-    for y in x:
+
+    trains = []
+    for y in x['tra-list']:
+        isLast = '★' if y['tra']['typ'] == 'Ласточка' else ''
         train = {
             'departure': y['tra']['dep']['tim'],
             'arrival': y['tra']['arr']['tim'],
             'from': removeRepeatingStations(reve[y['tra']['dep']['st']][0]),
             'to': removeRepeatingStations(reve[y['tra']['arr']['st']][0]),
-            'schedule': defineDays(y['tra']['sch']) + (' ★' if y['tra']['typ'] == 'Ласточка' else '') + defineTime(y['tra']['tr-tim']),
-            'change': y['tra']['cha']
+            'schedule': replacer(y['tra']['sch'], dayReplacers),
+            'time': isLast + ' ' + defineTime(y['tra']['tr-tim']),
+            'change': y['tra']['cha'].replace('. Уточните дату поездки', '')
         }
-        if train['schedule'].find('отменен') != -1:
-            continue
+        if train['schedule'].find('отменен') == -1:
+            trains.append(train)
 
-        if flag:
-            s = '{}\t{}\t{}\t{}'.format(train['departure'], train['schedule'], train['from'], train['to'])
-        else:
-            s = '{}\t{}\t{}\t{}\t{}\t{}'.format(train['departure'], train['arrival'], train['schedule'], train['from'], train['to'], train['change'].replace('. Уточните дату поездки', ''))
+    if flag:
+        fields = ['departure', 'schedule', 'time', 'from', 'to']
+    else:
+        fields = ['departure', 'arrival', 'schedule', 'time', 'from', 'to', 'change']
+    resultedFields = []
+    for f in fields:
+        isNeeded = False
+        for train in trains:
+            isNeeded = isNeeded or train[f] != ''
+        if isNeeded:
+            resultedFields.append(f)
 
-        print(re.sub(r'\t +', '\t', s))
+    for train in trains:
+        print('\t'.join(map(lambda f: train[f], resultedFields)))
 
     sys.stdout.close()
     subprocess.Popen(['mousepad', filename + '.out'])
@@ -99,7 +79,6 @@ st = os.path.dirname(os.path.abspath(__file__)) + '/stations.in'
 file = open(st, 'r')
 hash = defaultdict(list)
 reve = defaultdict(list)
-# isBalt = False
 for string in file:
     l = string[:-1].rsplit(' ', 1)
     hash[l[0]].append(l[1])
@@ -125,5 +104,5 @@ while i < len(args):
             arg2 += ' ' + args[i + 2]
             fl = 1
         for to in hash[arg2]:
-            parse(directory, 'http://www.tutu.ru/spb/rasp.php?st1=' + fro + '&st2=' + to + '&json' + today)
+            parse(directory, 'https://www.tutu.ru/spb/rasp.php?st1=' + fro + '&st2=' + to + '&json' + today)
     i += 2 + fl
