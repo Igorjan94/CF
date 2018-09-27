f82edbe1544028f7e39e92191b5bb04c042fb3f3
igorjan94 <igorjan94@mail.ru>
2018-09-27 19:18:45 +0300

    exif updater

---
+++ scripts/updateExif.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+lat="${1/[.,]/}"
+long="${2/[.,]/}"
+cd $3
+
+exiv2 -v\
+    -M"set Exif.GPSInfo.GPSLatitude $lat/1000000 0/1 0/1"\
+    -M"set Exif.GPSInfo.GPSLatitudeRef N"\
+    -M"set Exif.GPSInfo.GPSLongitude $long/1000000 0/1 0/1"\
+    -M"set Exif.GPSInfo.GPSLongitudeRef E" *.JPG *.jpeg *.MOV *.MP4;
