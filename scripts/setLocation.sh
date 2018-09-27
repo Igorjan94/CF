fdb5f5013e61c3188c6a0c52c84e5bf0ee6df503
igorjan94 <igorjan94@mail.ru>
2018-09-27 19:36:18 +0300

    ""

--- scripts/setLocation.sh
+++ scripts/setLocation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-dir=$2
+dir="$2"
 [[ -z "$dir" ]] && dir="."
 
 latlong=$(cat /home/igorjan/documents/geolocations.txt | grep $1 | awk '{$1=""; print $0}')
 echo $latlong
-updateExif.sh $latlong $dir
+updateExif.sh $latlong "$dir"
