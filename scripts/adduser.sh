5da0d09483c7983c4ac1f637134ac61c402dfde6
igorjan94 <igorjan94@mail.ru>
2018-11-20 01:59:28 +0300

    adduser

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -150,9 +150,16 @@ if [ "$needsDocker" ]; then
             pacman -Sy --noconfirm docker docker-compose
         else
             apt-get update
+            apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+            curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+            add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+            apt-get update
             apt-get install docker-ce
             curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-            sudo chmod +x /usr/local/bin/docker-compose
+            chmod +x /usr/local/bin/docker-compose
         fi
         if [ "$user" ]; then
             echo "Adding user '$user' in docker group..."
