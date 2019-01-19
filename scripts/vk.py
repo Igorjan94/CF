d5c3b3981cf467619ed0534e802f95c1d3fc6845
igorjan94 <igorjan94@mail.ru>
2019-01-19 03:53:53 +0300

    [vk] photo in attach from files

--- scripts/vk.py
+++ scripts/vk.py
@@ -1,4 +1,5 @@
 import requests
+import random
 
 def vk(method, params):
     if not 'v' in params: params['v'] = '5.92'
@@ -8,3 +9,14 @@ def vk(method, params):
         print(ret['error']['error_msg'])
         return ret['error']
     return ret['response']
+
+def getPhoto(url, path, peer_id):
+    files = {'photo': open(path, 'rb')}
+    attach = requests.post(url, files = files)
+    return vk('photos.saveMessagesPhoto', attach.json())[0]
+
+def sendPhoto(paths, peer_id):
+    url = vk('photos.getMessagesUploadServer', {'peer_id': peer_id})['upload_url']
+    photos = [getPhoto(url, path, peer_id) for path in paths.split(' ')]
+    strings = ['photo' + str(photo['owner_id']) + '_' + str(photo['id']) for photo in photos]
+    vk('messages.send', {'attachment': ','.join(strings), 'random_id': random.randint(0, 2 ** 31), 'peer_id': peer_id})
