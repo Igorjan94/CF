283eb42e9564573d005f3f98398880c60f39807f
Igor Kolobov <igorjan94@mail.ru>
2019-04-17 13:51:36 +0300

    1154

---
+++ CodeForce/1154/F.cpp
@@ -0,0 +1,109 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 07 August 2018 (&&, whole) {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FILENAME "input"
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef  valarray<int>    va;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define    pb    push_back
+#define whole(a) begin(a), end(a)
+#define   next   next__
+#define   prev   prev__
+#define  count   count__
+
+#define ints(a...)  int a; readln(a)
+#define wr(args...) err(#args, args)
+
+#define ttt12i template<class T1, class T2> inline
+#define  tthti template<class H, class...T> inline
+#define  ttta  template<class... Args> inline
+#define  ttti  template<class T> inline
+
+const int MOD = 1000000007;
+const int INTMAX = numeric_limits<int>::max();
+const ll LLMAX = numeric_limits<ll>::max();
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+ttti void writeln_range(T f,T s){if(f!=s)for(auto i=f;i!=s;++i)writeln(*i);}
+vector<string>split(string&s,string d){vector<string>v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
+ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
+
+//Igorjan
+//}}}
+
+void run()
+{
+    ints(n, m, k);
+    vi a(n);
+    vector<pii> b(m);
+    readln(a, b);
+    sort(whole(a));
+    a.resize(k);
+    vector<int> p(k + 1, 0);
+    fori1(k + 1)
+	p[i] = p[i - 1] + a[i - 1];
+    auto get = [&](int l, int r) {
+	return p[r + 1] - p[l];
+    };
+    sort(b.rbegin(), b.rend());
+    vector<int> dp(k + 1, MOD);
+    vector<int> cc(k + 1, 0);
+    dp[k] = 0;
+    cc[k] = 0;
+    ROF(i, k - 1, 0)
+    {
+	if (dp[i + 1] + a[i] < dp[i])
+	    dp[i] = dp[i + 1] + a[i],
+	    cc[i] = cc[i + 1];
+	FOR(j, cc[i], m)
+	    if (b[j].first - 1 <= i)
+		if (int add = get(i - b[j].first + b[j].second + 1, i), ind = i - b[j].first + 1; dp[i + 1] + add < dp[ind])
+		    dp[ind] = dp[i + 1] + add,
+		    cc[ind] = cc[i] + 1;
+    }
+    writeln(dp[0]);
+}
+
+//{{{
+int main()
+{
+#ifndef ONLINE_JUDGE
+    double time = clock();
+#endif
+    ios_base::sync_with_stdio(false);
+//    freopen(FILENAME".in", "r", stdin);
+//    freopen(FILENAME".out", "w", stdout);
+    run();
+#ifndef ONLINE_JUDGE
+    writeln("execution time =", (clock() - time) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttti   ostream&operator<<(ostream&os,vector<T>const&a);
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttti   ostream&operator<<(ostream&os,vector<T>const&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   ostream&operator<<(ostream&os,valarray<T>const&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti   istream&operator>>(istream&is,valarray<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
