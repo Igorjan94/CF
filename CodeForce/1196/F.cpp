29a78268610f53de2d89f492b654926629b9f61c
igorjan94 <igorjan94@mail.ru>
2019-07-29 00:23:29 +0300

    1196

---
+++ CodeForce/1196/F.cpp
@@ -0,0 +1,120 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 07 August 2018 (&&, whole) {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FILENAME "input"
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<long long, long long>   pii;
+typedef  valarray<int>    va;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define    pb    push_back
+#define whole(a) begin(a), end(a)
+#define   next   next__
+#define   prev   prev__
+#define  count   count__
+
+#define ints(a...)  int a; readln(a)
+#define wr(args...) err(#args, args)
+
+#define ttt12i template<class T1, class T2> inline
+#define  tthti template<class H, class...T> inline
+#define  ttta  template<class... Args> inline
+#define  ttti  template<class T> inline
+
+const int MOD = 1000000007;
+const int INTMAX = numeric_limits<int>::max();
+const ll LLMAX = numeric_limits<ll>::max();
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+ttti void writeln_range(T f,T s){if(f!=s)for(auto i=f;i!=s;++i)writeln(*i);}
+vector<string>split(string&s,string d){vector<string>v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
+ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
+
+//Igorjan
+//}}}
+
+void run()
+{
+    ints(n, m, k);
+    vector<vector<pii>> g(n);
+    set<tuple<int, int, int>> edges;
+    int x, y, w;
+    fori(m)
+        readln(x, y, w),
+        edges.emplace(w, --x, --y);
+    auto it = edges.begin();
+    fori(k)
+        if (it != edges.end())
+        {
+            auto [, x, y] = *it++;
+            g[x].pb({y, w});
+            g[y].pb({x, w});
+        }
+
+    multiset<ll> ans;
+    fori(n) if (g[i].size())
+    {
+        vector<ll> d(n, LLMAX); d[i] = 0;
+        set<pii> s;
+        s.insert({0, i});
+        int cnt = 0;
+        while (!s.empty())
+        {
+            auto [dist, u] = *s.begin();
+            s.erase(s.begin());
+            if (dist > d[u]) continue;
+            if (dist) cnt++, ans.insert(dist);
+            if (cnt == k) break;
+
+            for (auto& [v, w]: g[u])
+                if (d[v] > dist + w)
+                    d[v] = dist + w,
+                    s.insert({dist + w, v});
+        }
+    }
+    auto X = ans.begin();
+    fori(2 * k - 1) ++X;
+    writeln(*X);
+}
+
+//{{{
+int main()
+{
+#ifndef ONLINE_JUDGE
+    double time = clock();
+#endif
+    ios_base::sync_with_stdio(false);
+//    freopen(FILENAME".in", "r", stdin);
+//    freopen(FILENAME".out", "w", stdout);
+    run();
+#ifndef ONLINE_JUDGE
+    writeln("execution time =", (clock() - time) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttti   ostream&operator<<(ostream&os,vector<T>const&a);
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttti   ostream&operator<<(ostream&os,vector<T>const&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   ostream&operator<<(ostream&os,valarray<T>const&a){if(a.size())os<<a[0];else os<<"\n";fori1(a.size())os<<"\n "[is_fundamental<T>::value]<<a[i];return os;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti   istream&operator>>(istream&is,valarray<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
