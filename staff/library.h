1a45cebc2c51e8e47b4b84e1f51664f306751351
igorjan94 <igorjan94@mail.ru>
2019-10-07 20:19:42 +0300

    ok

--- staff/library.h
+++ staff/library.h
@@ -481,7 +481,7 @@ vector<int> multiply(const vector<int>& a, const vector<int>& b)
 }
 
 //Igorjanpoint
-#define pointtt template<typename T>
+#define pointtt template<typename T = int>
 //sorts only if z is corner point;
 #define sortByPolarAngle(v, z, T) sort(v.begin(), v.end(), [&z](point<T>& a, point<T>& b) {\
     int q = orientation(z, a, b); return q == 0 ? dist(z, a) < dist(z, b) : q == -1;\
@@ -490,25 +490,27 @@ vector<int> multiply(const vector<int>& a, const vector<int>& b)
 pointtt struct point
 {
     T x, y;
-    point(T _x, T _y) : x(_x), y(_y) {}
     point(){}
-    point operator+(point b) { return point(x + b.x, y + b.y); }
-    point operator-() { return point(-x, -y); }
-    T operator*(point b) { return x * b.x + y * b.y; }
-    T operator^(point b) { return x * b.y - y * b.x; }
-    T operator!() { return x * x + y * y; }
-    bool operator<(point b) { return x == b.x ? y < b.y : x < b.x; }
+    point(T _x, T _y) : x(_x), y(_y) {}
+    point operator=(const point& b) { x = b.x; y = b.y; }
+    point operator+(const point& b) const { return point(x + b.x, y + b.y); }
+    point operator-(const point& b) const { return point(x - b.x, y - b.y); }
+    point operator-() const { return point(-x, -y); }
+    T operator*(const point& b) const { return x * b.x + y * b.y; }
+    T operator^(const point& b) const { return x * b.y - y * b.x; }
+    T operator!() const { return x * x + y * y; }
+    bool operator<(const point& b) const { return x == b.x ? y < b.y : x < b.x; }
 };
 pointtt istream&operator>>(istream&is,point<T>&a){return is>>a.x>>a.y;}
 pointtt ostream&operator<<(ostream&os,point<T>&a){return os<<a.x<<" "<<a.y;}
-pointtt T dist(point<T>&a,point<T>&b){return!point<T>(a+-b);}
+pointtt T dist(const point<T>&a,const point<T>&b){return!point<T>(a-b);}
 //dist from point C to line AB equals to answer.first / sqrt(answer.second);
-pointtt pair<T,T> dist(point<T>&a,point<T>&b,point<T>&c){return{abs((a+-b)*c)+(a^b),dist(a,b)};}
-pointtt int orientation(point<T>&a,point<T>&b,point<T>&c){T q=a.x*b.y-a.y*b.x-a.x*c.y+a.y*c.x+b.x*c.y-b.y*c.x;return q>0?1:q<0?-1:0;}
+pointtt pair<T,T> dist(const point<T>&a,const point<T>&b,const point<T>&c){return{abs((a-b)*c)+(a^b),dist(a,b)};}
+pointtt int orientation(const point<T>&a,const point<T>&b,const point<T>&c){T q=a.x*b.y-a.y*b.x-a.x*c.y+a.y*c.x+b.x*c.y-b.y*c.x;return q>0?1:q<0?-1:0;}
 
 //IgorjanconvexHull
-pointtt vector<point<T>>convexHull(vector<point<T>>a){sort(a.begin(),a.end());int n=a.size(),j=-1,k=0;ROF(i,n-2,0)a.push_back(a[i]);fori(a.size()){for(;j>k&&orientation(a[j-1],a[j],a[i])!=1;--j)
-    ;a[++j]=a[i];if(!k&&i==n-1)k=j;}a.resize(j);return a;}
+pointtt void convexHull(vector<point<T>>&a){sort(a.begin(),a.end());int n=a.size(),j=-1,k=0;ROF(i,n-2,0)a.push_back(a[i]);fori(a.size()){for(;j>k&&orientation(a[j-1],a[j],a[i])!=1;--j)
+    ;a[++j]=a[i];if(!k&&i==n-1)k=j;}a.resize(j);}
 
 //IgorjanprintAns
 ttta void printAnswerAndExit(Args... args){writeln(args...);exit(0);}
---
+++ temp/geo.cpp
@@ -0,0 +1,113 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 14 september 2019 (writeln<T>, main) {{{
+#include "geodeb/lib/geodeb.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define    pb    push_back
+#define whole(a) begin(a), end(a)
+#define   next   next__
+#define   prev   prev__
+#define  count   count__
+
+#define ints(a...)  int a; readln(a)
+#define wr(args...) err(#args, args)
+
+#define ttt12i template<class T1, class T2> inline
+#define  tthti template<class H, class...T> inline
+#define  ttta  template<class... Args> inline
+#define  ttti  template<class T> inline
+
+const int MOD = 1000000007;
+const int INTMAX = numeric_limits<int>::max();
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+vector<string>split(string&s,string d){vector<string>v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
+ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
+
+//Igorjan
+
+//point
+#define pointtt template<typename T = int>
+//sorts only if z is corner point;
+#define sortByPolarAngle(v, z, T) sort(v.begin(), v.end(), [&z](point<T>& a, point<T>& b) {\
+    int q = orientation(z, a, b); return q == 0 ? dist(z, a) < dist(z, b) : q == -1;\
+});
+
+pointtt struct point
+{
+    T x, y;
+    point(){}
+    point(T _x, T _y) : x(_x), y(_y) {}
+    point operator+(const point& b) const { return point(x + b.x, y + b.y); }
+    point operator-(const point& b) const { return point(x - b.x, y - b.y); }
+    point operator-() const { return point(-x, -y); }
+    T operator*(const point& b) const { return x * b.x + y * b.y; }
+    T operator^(const point& b) const { return x * b.y - y * b.x; }
+    T operator!() const { return x * x + y * y; }
+    bool operator<(const point& b) const { return x == b.x ? y < b.y : x < b.x; }
+};
+pointtt istream&operator>>(istream&is,point<T>&a){return is>>a.x>>a.y;}
+pointtt ostream&operator<<(ostream&os,point<T>&a){return os<<a.x<<" "<<a.y;}
+pointtt T dist(const point<T>&a,const point<T>&b){return!point<T>(a-b);}
+//dist from point C to line AB equals to answer.first / sqrt(answer.second);
+pointtt pair<T,T> dist(const point<T>&a,const point<T>&b,const point<T>&c){return{abs((a-b)*c)+(a^b),dist(a,b)};}
+pointtt int orientation(const point<T>&a,const point<T>&b,const point<T>&c){T q=a.x*b.y-a.y*b.x-a.x*c.y+a.y*c.x+b.x*c.y-b.y*c.x;return q>0?1:q<0?-1:0;}
+
+//convexHull
+pointtt void convexHull(vector<point<T>>&a){sort(a.begin(),a.end());int n=a.size(),j=-1,k=0;ROF(i,n-2,0)a.push_back(a[i]);fori(a.size()){for(;j>k&&orientation(a[j-1],a[j],a[i])!=1;--j)
+    ;a[++j]=a[i];if(!k&&i==n-1)k=j;}a.resize(j);}
+//}}}
+
+void run()
+{
+    GD_INIT("kek.html");
+    GD_SET_PRECISION(0);
+    ints(n);
+    vector<point<>> a(n);
+    readln(a);
+    for (auto& p: a)
+        GD_POINT(p);
+    GD_PAUSE();
+    convexHull(a);
+    fori(n - 1)
+        GD_SEGMENT(a[i], a[i + 1], "bold");
+    GD_PAUSE();
+    fori(n)
+        GD_CIRCLE(a[i], 1, "red:blue");
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
