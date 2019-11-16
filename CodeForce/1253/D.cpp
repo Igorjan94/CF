a7175bd4a50afa8187af432465781ecb1d2b8e03
igorjan94 <igorjan94@mail.ru>
2019-11-16 22:52:51 +0300

    1253

---
+++ CodeForce/1253/D.cpp
@@ -0,0 +1,125 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 14 september 2019 (writeln<T>, main) {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define    pb    push_back
+#define whole(a) begin(a), end(a)
+#define   next   next__
+#define   prev   prev__
+#define  count   count__
+
+#define ints(a...)  int a; readln(a)
+#define wr(args...) err(#args, args)
+
+#define ttt12i template<class T1, class T2> inline
+#define  tthti template<class H, class...T> inline
+#define  ttta  template<class... Args> inline
+#define  ttti  template<class T> inline
+
+const int MOD = 1000000007;
+const int INTMAX = numeric_limits<int>::max();
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+ttta void readln(Args&... args){(read(args),...);}tthti void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+vector<string>split(string&s,string d){vector<string>v;size_t p=0;while((p=s.find(d))!=string::npos)v.pb(s.substr(0,p)),s.erase(0,p+d.length());v.pb(s);return v;}
+ttta void err(string v,Args...args){auto vv=split(v,", ");auto it=vv.begin();(writeln(*it++,"=",args),...);}
+
+//Igorjan
+//umax
+template<typename T1, typename T2>
+T1 umax(T1& a, T2 b)
+{
+    return a < T2(b) ? a = b : a;
+}
+
+//dsu
+struct dsu
+{
+    vector<int> a, size, mx;
+
+    dsu(int n)
+    {
+        a.resize(n);
+        size.resize(n, 1);
+        mx.resize(n);
+        iota(a.begin(), a.end(), 0);
+        iota(mx.begin(), mx.end(), 0);
+    }
+
+    int operator[](int i)
+    {
+        return get(i);
+    }
+
+    int get(int i)
+    {
+        return a[i] == i ? i : a[i] = get(a[i]);
+    }
+
+    bool uni(int i, int j)
+    {
+        i = get(i);
+        j = get(j);
+        if (i == j) return false;
+        if (size[i] > size[j])
+            swap(i, j);
+        a[i] = j;
+        size[j] += size[i];
+        umax(mx[j], mx[i]);
+        return true;
+    }
+};
+
+//}}}
+
+void run()
+{
+    ints(n, m);
+    dsu d(n);
+    vector<bool> used(n, false);
+    int x, y, ans = 0;
+    fori(m)
+        readln(x, y),
+        d.uni(--x, --y);
+    fori(n)
+        if (!used[i])
+            for (int j = i; j <= d.mx[d[i]]; ++j)
+                ans += d.uni(i, j),
+                used[j] = true;
+    writeln(ans);
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
