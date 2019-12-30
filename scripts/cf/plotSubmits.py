3bec531e8cba4ef367b13eb3137738bd59a62309
igorjan94 <igorjan94@mail.ru>
2019-12-30 04:03:17 +0300

    plotSubmits

--- scripts/cf/plotSubmits.py
+++ scripts/cf/plotSubmits.py
@@ -18,13 +18,17 @@ def getSubmits(contestId):
     return unlimited(getter, 'result', 10000, True, count_field = 'count', offset_field = 'from', starting_offset = 1, contestId = contestId)
 
 
-def plotContest(contestId):
+@completion.command()
+@click.argument('contest_id', required=True, type=int, nargs=1)
+@click.option('-m', '--minutes', help='Stat for last <minutes>', default=20)
+@click.option('-u', '--unofficial', is_flag=True, help='Show only unofficial participants', default=False)
+def plotContest(contest_id, minutes, unofficial):
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
-    contest = cf('contest.standings', **{'from': 1, 'contestId': contestId, 'count': 1}).result.contest
+    contest = cf('contest.standings', **{'from': 1, 'contestId': contest_id, 'count': 1}).result.contest
     duration = contest.durationSeconds // 60
     title = contest.name
-    submits = getSubmits(contestId)
+    submits = getSubmits(contest_id)
 
     def getDate(submit):
         return submit.relativeTimeSeconds / 60
@@ -39,26 +43,29 @@ def plotContest(contestId):
         return len(list(filter(lambda d: d >= duration - minutes, submits))) * 100
 
     def add(submits, constestants):
-        MINUTES = 20
         all_submits = list(map(getDate, filter(lambda submit: submit.verdict != 'OK', submits)))
         ok_submits = list(map(getDate, filter(lambda submit: submit.verdict == 'OK', submits)))
         dates.append(ok_submits)
         dates.append(all_submits)
-        count_ok_last_minutes = getLast(ok_submits, MINUTES)
-        count_all_last_minutes = getLast(all_submits, MINUTES)
-        legend.append(f'{constestants} OK ({len(ok_submits)}), {count_ok_last_minutes / len(ok_submits):.0f}% in last {MINUTES} minutes')
-        legend.append(f'{constestants} Incorrect ({len(all_submits)}), {count_all_last_minutes / len(all_submits):.0f}% in last {MINUTES} minutes')
+        count_ok_last_minutes = getLast(ok_submits, minutes)
+        count_all_last_minutes = getLast(all_submits, minutes)
+        legend.append(f'{constestants} OK ({len(ok_submits)}), {count_ok_last_minutes / len(ok_submits):.0f}% in last {minutes} minutes')
+        legend.append(f'{constestants} Incorrect ({len(all_submits)}), {count_all_last_minutes / len(all_submits):.0f}% in last {minutes} minutes')
 
-    add(off, 'official')
-    # add(unoff, 'unofficial')
+    if unofficial:
+        add(unoff, 'unofficial')
+    else:
+        add(off, 'official')
 
     fig, ax = plt.subplots(1, 1)
     ax.hist(dates, bins=duration, stacked=True)
     ax.legend(legend)
-    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    # ax.xaxis.set_major_formatter(mdates.AutoDateFormatter())
     plt.xlabel('Time')
     plt.ylabel('Count of submits')
     plt.title(title)
-    plt.show()
+    plt.savefig(title.replace(r'\s', '') + '.png', dpi=900);
+    # plt.show()
 
-plotContest(1249)
+if __name__ == "__main__":
+    completion()
