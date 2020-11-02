3f92d66958cbc32fecb01b493395899f2db269b8
igorjan94 <igorjan94@mail.ru>
2020-11-02 20:52:17 +0300

    1442

---
+++ CodeForce/1442/B.cpp
@@ -0,0 +1,129 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 18 march 2020 (writeln<tuple>, whole->all) {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define ints(a...) int a; readln(a)
+
+[[maybe_unused]] const int MOD = 1000000007;
+[[maybe_unused]] const int INTMAX = numeric_limits<int>::max();
+
+#define ttt12i template<class T1, class T2> inline
+#define  ttti  template<class T> inline
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+template<class... Args> inline void readln(Args&... args){(read(args),...);}
+template<class H, class...T> inline void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+
+//Igorjan
+//modular
+template<typename T = int, T mod = 998244353>
+struct modular
+{
+    T value;
+
+    modular() : value(0) {}
+    modular(const modular& other) : value(other.value) {}
+    modular operator=(const modular& other) { value = other.value; return *this; }
+    template<typename T1> modular operator=(const T1& other) { value = other % mod; if (value < 0) value += mod; return *this; }
+    template<typename T1> modular(T1 const& other) { value = other % mod; if (value < 0) value += mod; }
+    template<typename T1> modular(T1 const& num, T1 const& den) { *this = modular(den) ^ (mod - 2) * num; }
+    template<typename T1> modular& operator^=(T1 const& deg) { modular a(*this); for (T1 n = deg - 1; n > 0; n >>= 1) { if (n & 1) *this *= a; a *= a; } return *this; }
+    template<typename T1> modular  operator^ (T1 const& deg) const { return modular(*this) ^= deg; }
+    inline modular& operator+=(modular const& t)       { value += t.value; if (value > mod) value -= mod; return *this; }
+    inline modular& operator-=(modular const& t)       { value -= t.value; if (value < 0  ) value += mod; return *this; }
+    inline modular& operator*=(modular const& t)       { value = (value * 1ll * t.value) % mod; return *this; }
+    inline modular& operator/=(modular const& t)       { return *this *= ~t; }
+    inline modular  operator+ (modular const& t) const { return modular(*this) += t; }
+    inline modular  operator- (modular const& t) const { return modular(*this) -= t; }
+    inline modular  operator* (modular const& t) const { return modular(*this) *= t; }
+    inline modular  operator/ (modular const& t) const { return modular(*this) /= t; }
+    inline modular  operator~ (                ) const { return modular(T(1), value); }
+    inline bool     operator==(modular const& t) const { return value == t.value; }
+    inline bool     operator!=(modular const& t) const { return value != t.value; }
+    explicit operator T() const { return value; }
+
+    inline friend ostream& operator<<(ostream& os, modular const& m) { return os << m.value; }
+    inline friend istream& operator>>(istream& is, modular& m) { return is >> m.value; m.value %= mod; if (m.value < 0) m.value += mod; }
+};
+
+//}}}
+
+void run()
+{
+    ints(n, k);
+    vi a(n), b(k);
+    readln(a, b);
+    vector<int> p(n + 1);
+    set<pii> u, l;
+    fori(n)
+        p[a[i]] = i,
+        u.insert({i, a[i]}),
+        l.insert({-i, a[i]});
+    set<int> exists(all(b));
+    map<int, int> twins;
+
+    modular<> ans = 1;
+    vector<vector<int>> moves;
+    for (const int& x: b)
+    {
+        moves.pb({});
+        int i = p[x];
+        auto upper = u.upper_bound({i, n + 10});
+        auto lower = l.upper_bound({-i, n + 10});
+        if (upper != u.end())
+            if (!exists.count(upper->second))
+                moves.back().pb(upper->second);
+        if (lower != l.end())
+            if (!exists.count(lower->second))
+                moves.back().pb(lower->second);
+        int d = moves.back().size();
+        ans *= d;
+        twins[x] += d;
+        //writeln(x, moves.back());
+        for (int y: moves.back())
+            if (--twins[y] <= 0)
+            {
+                u.erase({p[y], n + 10});
+                l.erase({-p[y], n + 10});
+            }
+        exists.erase(x);
+    }
+    writeln(ans);
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ints(t); fori(t) run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
