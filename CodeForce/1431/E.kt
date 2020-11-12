afbbe6afdc8512d89a461546ad50acd448edd687
igorjan94 <igorjan94@mail.ru>
2020-11-12 20:33:55 +0300

    1431

---
+++ CodeForce/1431/E.kt
@@ -0,0 +1,82 @@
+import java.io.PrintWriter// {{{
+import kotlin.math.*
+import kotlin.collections.*// }}}
+
+private fun run() {
+    val (n) = readln()
+    var a = readln()
+    var b = readln()
+
+    var ans = -1
+    var best = -1
+    for (i in 0..n - 1) {
+        var cur = 1000000000
+        for (j in 0..n - 1)
+            cur = min(cur, abs(a[j] - b[(j + i) % n]))
+        if (cur > best) {
+            best = cur
+            ans = i
+        }
+    }
+    var temp = IntArray(n, {-1})
+    for (i in 0..n - 1)
+        temp[i] = (ans + i) % n + 1
+
+    writeln(temp.joinToString(" "))
+}
+
+private fun PrintWriter.readSolveWrite() {
+    val (t) = readln()
+    for (q in 1..t) {
+        run()
+    }
+}
+
+private fun ok(x: Boolean) = if (x) 1 else 0// {{{
+
+private fun writeln(vararg strings: Any) = println(strings.map{if (it is IntArray) it.joinToString(" ") else it}.joinToString(" "))
+
+private fun readln() = getIntArray()
+
+private fun getIntArray() = readLine()!!.splitToIntArray()
+
+private fun bufferOut(block: PrintWriter.() -> Unit) = PrintWriter(System.out).use { block(it) }
+
+data class Pt(val x: Int, val y: Int, val i: Int, var ans: Int)
+
+fun main() = bufferOut { readSolveWrite() }
+
+private fun String.splitToIntArray(): IntArray {
+    val n = length
+    if (n == 0) return IntArray(0) // EMPTY
+    var res = IntArray(4)
+    var m = 0
+    var i = 0
+    while (true) {
+        var cur = 0
+        var neg = false
+        var c = get(i) // expecting number, IOOB if there is no number
+        if (c == '-') {
+            neg = true
+            i++
+            c = get(i) // expecting number, IOOB if there is no number
+        }
+        while (true) {
+            val d = c.toInt() - '0'.toInt()
+            require(d in 0..9) { "Unexpected character '$c' at $i" }
+            require(cur >= Integer.MIN_VALUE / 10) { "Overflow at $i" }
+            cur = cur * 10 - d
+            require(cur <= 0) { "Overflow at $i" }
+            i++
+            if (i >= n) break
+            c = get(i)
+            if (c == ' ') break
+        }
+        if (m >= res.size) res = res.copyOf(res.size * 2)
+        res[m++] = if (neg) cur else (-cur).also { require(it >= 0) { "Overflow at $i" } }
+        if (i >= n) break
+        i++
+    }
+    if (m < res.size) res = res.copyOf(m)
+    return res
+}// }}}
