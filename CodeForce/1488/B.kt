c129e7d798f61d29e0da79e1e2eb42933a3875f6
igorjan94 <igorjan94@mail.ru>
2021-03-10 01:19:03 +0300

    1488

---
+++ CodeForce/1488/B.kt
@@ -0,0 +1,75 @@
+import java.io.PrintWriter// {{{
+import kotlin.math.*
+import kotlin.collections.*// }}}
+
+private fun run() {
+    var (n, k) = readln()
+    val s = readLine()!!
+    var b = 0
+    var zeros = 0
+    for (i in s.indices)
+        if (s[i] == '(')
+            b++
+        else {
+            if (--b == 0)
+                zeros++
+        }
+    n /= 2
+    var ans = min(n, zeros + k)
+    writeln(ans)
+}
+
+private fun PrintWriter.readSolveWrite() {
+    val (t) = readln()
+    for (q in 1..t) {
+        run()
+    }
+}
+
+private fun ok(x: Boolean) = if (x) 1 else 0// {{{
+
+private fun writeln(vararg strings: Any) =
+    println(strings.map { if (it is IntArray) it.joinToString(" ") else it }.joinToString(" "))
+
+private fun readln() = getIntArray()
+
+private fun getIntArray() = readLine()!!.splitToIntArray()
+
+private fun bufferOut(block: PrintWriter.() -> Unit) = PrintWriter(System.out).use { block(it) }
+
+fun main() = bufferOut { readSolveWrite() }
+
+private fun String.splitToIntArray(): IntArray {
+    val n = length
+    if (n == 0) return IntArray(0) // EMPTY
+    var res = IntArray(4)
+    var m = 0
+    var i = 0
+    while (true) {
+        var cur = 0
+        var neg = false
+        var c = get(i) // expecting number, IOOB if there is no number
+        if (c == '-') {
+            neg = true
+            i++
+            c = get(i) // expecting number, IOOB if there is no number
+        }
+        while (true) {
+            val d = c.toInt() - '0'.toInt()
+            require(d in 0..9) { "Unexpected character '$c' at $i" }
+            require(cur >= Integer.MIN_VALUE / 10) { "Overflow at $i" }
+            cur = cur * 10 - d
+            require(cur <= 0) { "Overflow at $i" }
+            i++
+            if (i >= n) break
+            c = get(i)
+            if (c == ' ') break
+        }
+        if (m >= res.size) res = res.copyOf(res.size * 2)
+        res[m++] = if (neg) cur else (-cur).also { require(it >= 0) { "Overflow at $i" } }
+        if (i >= n) break
+        i++
+    }
+    if (m < res.size) res = res.copyOf(m)
+    return res
+}// }}}
