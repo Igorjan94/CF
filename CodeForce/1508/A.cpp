fbe0b014e34294b06483412b8108fda92a3e7e87
igorjan94 <igorjan94@mail.ru>
2021-04-16 20:39:15 +0300

    1508

---
+++ CodeForce/1508/A.cpp
@@ -0,0 +1,113 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 18 march 2020 (writeln<tuple>, whole->all) {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define ints(a...) int a; readln(a)
+
+[[maybe_unused]] const int MOD = 1000000007;
+[[maybe_unused]] const int INTMAX = numeric_limits<int>::max();
+
+#define ttt12i template<class T1, class T2> inline
+#define  ttti  template<class T> inline
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+template<class... Args> inline void readln(Args&... args){(read(args),...);}
+template<class H, class...T> inline void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+
+//Igorjan
+//}}}
+
+void run()
+{
+    ints(n);
+    int N = n * 3;
+    n *= 2;
+
+    auto check = [&](const string& s, const string& t, char f) -> string {
+        string a;
+        int i = 0;
+        int j = 0;
+        while (i < n || j < n)
+        {
+            int c1 = 0;
+            int c2 = 0;
+            while (i < n && s[i] != f) ++i, ++c1;
+            while (j < n && t[j] != f) ++j, ++c2;
+            forn(q, max(c1, c2))
+                a.pb(f ^ 1);
+            if (i < n || j < n)
+                a.pb(f);
+            ++i; ++j;
+        }
+        return a;
+    };
+
+    vector<string> s(3);
+    readln(s);
+    for (char c : "01")
+        fori(3)
+            forj(3)
+                if (i != j)
+                {
+                    string t = check(s[i], s[j], c);
+                    if (SZ(t) <= N)
+                    {
+                        int q = 0;
+                        int w = 0;
+                        forn(k, SZ(t))
+                        {
+                            if (q < n && s[i][q] == t[k]) ++q;
+                            if (w < n && s[j][w] == t[k]) ++w;
+                        }
+                        if (q != n || w != n)
+                        {
+                            writeln();
+                            writeln(s[i]);
+                            writeln(s[j]);
+                            writeln(t);
+                            exit(2);
+                        }
+                        return writeln(t);
+                    }
+                }
+    writeln();
+    writeln(s);
+    exit(1);
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ints(t); fori(t) run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
