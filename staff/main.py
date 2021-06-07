144b38eb50301d6df52960ad792aa4f67f6b58fc
igorjan94 <igorjan94@mail.ru>
2021-06-08 02:59:44 +0300

    really fast python io

--- staff/main.py
+++ staff/main.py
@@ -1,38 +1,87 @@
+import os
 import sys
-import functools
 
-def main():
-    writeln_range(sorted(read_int_array()))
+if 'PyPy' in sys.version: # {{{
+    from _continuation import continulet
+else:
+    import threading
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
 
 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
 
 
-
-
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+# }}}
 
 def read():
-    return sys.stdin.readline().rstrip('\n')
+    return list(map(int, input().split()))
 
-def read_array(sep=None, maxsplit=-1):
-    return read().split(sep, maxsplit)
+def main():
+    [n] = read()
+    a = read()
+    print(*sorted(a))
 
-def read_int():
-    return int(read())
+if __name__ == '__main__': # {{{
+    if 'PyPy' in sys.version:
 
-def read_int_array(sep=None, maxsplit=-1):
-    return list(map(int, read_array(sep, maxsplit)))
+        def bootstrap(cont):
+            call, arg = cont.switch()
+            while True:
+                call, arg = cont.switch(to=continulet(lambda _, f, args: f(*args), call, arg))
 
-def writeln(*args, **kwargs):
-    sep = kwargs.get('sep', ' ')
-    end = kwargs.get('end', '\n')
-    sys.stdout.write(sep.join(str(a) for a in args) + end)
+        cont = continulet(bootstrap)
+        cont.switch()
 
-def writeln_range(array, **kwargs):
-    sep = kwargs.get('sep', ' ')
-    end = kwargs.get('end', '\n')
-    sys.stdout.write(sep.join(str(a) for a in array) + end)
+        main()
 
-if __name__ == '__main__':
-    main()
+    else:
+        sys.setrecursionlimit(1 << 30)
+        threading.stack_size(1 << 27)
 
-#@functools.lru_cache(None)
+        main_thread = threading.Thread(target=main)
+        main_thread.start()
+        main_thread.join() # }}}
