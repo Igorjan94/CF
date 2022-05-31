53e821d34f64e687f950f76da3369c235d5a9651
igorjan94 <igorjan94@mail.ru>
2022-05-31 20:49:56 +0300

    1691

---
+++ CodeForce/1691/D.cpp
@@ -0,0 +1,128 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 18 march 2020 (writeln<tuple>, whole->all) {{{
+#include <bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#endif
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define ints(a...) int a; readln(a)
+
+[[maybe_unused]] const int MOD = 1000000007;
+[[maybe_unused]] const int INTMAX = numeric_limits<int>::max();
+
+#define ttt12i template<class T1, class T2> inline
+#define  ttti  template<class T> inline
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+template<class... Args> inline void readln(Args&... args){(read(args),...);}
+template<class H, class...T> inline void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+
+//Igorjan
+//sparseTable
+//0-indexed, [l, r)
+template<typename T>
+struct sparseTable
+{
+    int n;
+    vector<vector<T>> st;
+    vector<int> logs;
+    typedef function<T (T, T)> F;
+    F f;
+
+    int highestBit(int x) const { return 31 - __builtin_clz(x); }
+
+    sparseTable() {}
+
+    sparseTable(vector<T>& a, F g)
+    {
+        n = a.size();
+        f = g;
+
+        logs.push_back(0);
+        logs.push_back(0);
+        FOR(i, 2, n + 1) logs.push_back(logs[i / 2] + 1);
+        int L = logs.back() + 1;
+        st.resize(L, vector<T>(n));
+        fori(n)
+            st[0][i] = a[i];
+        FOR(k, 1, L)
+            for (int i = 0; i + (1 << k) <= n; i++)
+                st[k][i] = f(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
+    }
+
+    T get(int l, int r)
+    {
+        ++r;
+        int len = highestBit(r - l);
+        return f(st[len][l], st[len][r - (1 << len)]);
+    }
+};
+
+//}}}
+
+void run()
+{
+    ints(n);
+    vi a(n);
+    readln(a);
+    vector<ll> p = {0};
+    fori(n)
+        p.pb(p.back() + a[i]);
+    vector<int> b(n);
+    iota(all(b), 0);
+    sparseTable<int> mxa(b, [&](const int& x, const int& y) {
+        return a[x] < a[y] ? y : x;
+    });
+    sparseTable<ll> prevMn(p, [&](const ll& x, const ll& y) { return min(x, y);});
+    sparseTable<ll> prevMx(p, [&](const ll& x, const ll& y) { return max(x, y);});
+    bool ok = true;
+    auto dfs = [&](auto dfs, int l, int r) {
+        if (l > r) return;
+        int m = mxa.get(l, r);
+        ll mn = prevMn.get(l, m);
+        ll mx = prevMx.get(m + 1, r + 1);
+        if (a[m] < mx - mn)
+            ok = false;
+        dfs(dfs, l, m - 1);
+        dfs(dfs, m + 1, r);
+    };
+    dfs(dfs, 0, n - 1);
+    writeln(ok ? "YES" : "NO");
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ints(t); fori(t) run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
