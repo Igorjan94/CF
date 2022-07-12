8d1eb48548b9a4f900b3d92960f474a52e2a054f
igorjan94 <igorjan94@mail.ru>
2022-07-12 19:25:58 +0300

    1703

--- CodeForce/1703/B.py
+++ CodeForce/1703/B.py
@@ -1,12 +1,2 @@
-t = int(input())
-for q in range(t):
-    n = int(input())
-    s = input()
-    solved = set()
-    ans = 0
-    for c in s:
-        ans += 1
-        if c not in solved:
-            ans += 1
-        solved.add(c)
-    print(ans)
+f=input
+for q in range(int(f())):n=f();s=f();print(len(s)+len(set(s)))
