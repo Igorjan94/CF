84e923f6c8ea885ddd09f9af0308fbe733ffcd07
igorjan94 <igorjan94@mail.ru>
2022-08-01 20:45:58 +0300

    1714

---
+++ CodeForce/1714/F.cpp
@@ -0,0 +1,100 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 18 march 2020 (writeln<tuple>, whole->all) {{{
+#include <bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#endif
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define ints(a...) int a; readln(a)
+
+[[maybe_unused]] const int MOD = 1000000007;
+[[maybe_unused]] const int INTMAX = numeric_limits<int>::max();
+
+#define ttt12i template<class T1, class T2> inline
+#define  ttti  template<class T> inline
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+template<class... Args> inline void readln(Args&... args){(read(args),...);}
+template<class H, class...T> inline void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+
+//Igorjan
+//}}}
+
+void run()
+{
+    ints(n, d12, d23, d13);
+    int x = d23 + d13 - d12;
+    if (x % 2 != 0 || d12 > d13 + d23 || d13 > d12 + d23 || d23 > d13 + d12)
+        return writeln("NO");
+    x /= 2;
+    int neededVertices = d12 - 2 + x;
+    if (neededVertices + 3 > n)
+        return writeln("NO");
+    int k = d13 - x;
+    vector<int> vertices(d12 + 1);
+    iota(all(vertices), 3);
+    vertices[0] = 1;
+    vertices.back() = 2;
+    if (x == 0)
+        vertices.erase(vertices.begin() + d12 - 1),
+        vertices.insert(vertices.begin() + k, 3);
+
+    set<int> used;
+    fori1(n + 1)
+        used.insert(i);
+    for (int x: vertices)
+        used.erase(x);
+    used.erase(3);
+    vector<int> path = {vertices[k]};
+    fori(x - 1)
+        path.pb(*used.begin()),
+        used.erase(used.begin());
+    if (x != 0)
+        path.pb(3);
+
+
+    writeln("YES");
+    fori1(vertices.size())
+        writeln(vertices[i - 1], vertices[i]);
+    fori1(path.size())
+        writeln(path[i - 1], path[i]);
+    for (int left: used)
+        writeln(1, left);
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ints(t); fori(t) run();
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
