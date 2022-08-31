9d62ebc5f3a39ca447ff50ac221d035c38020440
igorjan94 <igorjan94@mail.ru>
2022-08-31 09:47:19 +0300

    Statemen\t

---
+++ CodeForce/396147/A.py
@@ -0,0 +1,22 @@
+n = int(input())
+if n < 1200:
+    print('Newbie')
+elif n < 1400:
+    print('Pupil')
+elif n < 1600:
+    print('Specialist')
+elif n < 1900:
+    print('Expert')
+elif n < 2100:
+    print('Candidate Master')
+elif n < 2300:
+    print('Master')
+elif n < 2400:
+    print('International Master')
+elif n < 2600:
+    print('Grandmaster')
+elif n < 3000:
+    print('International Grandmaster')
+else:
+    print('Legendary Grandmaster')
+
