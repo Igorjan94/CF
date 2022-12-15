f7f53c3bcbd3d1563822d2f94769e80eb27de973
igorjan94 <igorjan94@mail.ru>
2022-12-15 20:27:41 +0300

    1762

---
+++ CodeForce/1762/D.cpp
@@ -0,0 +1,134 @@
+// Igorjan94, template version from 13 October 2017. C++17 version, modified 18 march 2020 (writeln<tuple>, whole->all) {{{
+#include <bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#endif
+
+using namespace std;
+
+#define FOR(i, m, n) for (int i = m; i <  (int) (n); ++i)
+#define ROF(i, m, n) for (int i = m; i >= (int) (n); --i)
+#define forn(i, n)   for (int i = 0; i < (int) (n); ++i)
+#define  fori1(n)    for (int i = 1; i < (int) (n); ++i)
+#define  forj1(n)    for (int j = 1; j < (int) (n); ++j)
+#define   fori(n)    for (int i = 0; i < (int) (n); ++i)
+#define   forj(n)    for (int j = 0; j < (int) (n); ++j)
+#define     SZ(a)    int(size(a))
+
+typedef  pair<int, int>   pii;
+typedef   vector<int>     vi;
+typedef    long long      ll;
+
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define ints(a...) int a; readln(a)
+
+[[maybe_unused]] const int MOD = 1000000007;
+[[maybe_unused]] const int INTMAX = numeric_limits<int>::max();
+
+#define ttt12i template<class T1, class T2> inline
+#define  ttti  template<class T> inline
+
+void writeln(){cout<<"\n";}ttti void print(T&& a);ttti void priws(T&& a);ttti void read(T& a);
+template<class... Args> inline void readln(Args&... args){(read(args),...);}
+template<class H, class...T> inline void writeln(H&& h,T&&...t){priws(h);(print(t),...);writeln();}
+
+//Igorjan
+//rng
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+//}}}
+
+int run()
+{
+    //cerr << "-------------------" << endl;
+    ints(n);
+    map<pii, int> cache;
+    auto ask = [&](int i, int j, bool ans = false) {
+        if (i > j) swap(i, j);
+        //cerr << "ASK " << i << " " << j << endl;
+        if (i == j) return 1;
+        pii t = {i, j};
+        if (!ans)
+        {
+            if (auto it = cache.find(t); it != cache.end())
+                return it->second;
+        }
+        writeln(ans ? "!" : "?", i + 1, j + 1);
+        cout.flush();
+        ints(x);
+        return cache[t] = x;
+    };
+    if (n == 2)
+        return ask(0, 1, true);
+    if (n <= 5)
+    {
+        fori(n)
+            forj(n)
+            {
+                int q = ask(i, j);
+                if (q == n - 1)
+                    return ask(i, j, true);
+            }
+    }
+
+    int t = 1;
+    vector<int> cur(n);
+    iota(all(cur), 0);
+    while (cur.size() > 2)
+    {
+        int sz = SZ(cur);
+        int x, y, temp;
+        shuffle(all(cur), rng);
+        x = cur.back();
+        fori(min(2, sz))
+        {
+            y = cur[i];
+            temp = ask(x, y);
+            if (temp > t)
+                break;
+        }
+        if (temp == t)
+        {
+            cur.pop_back();
+            continue;
+        }
+        set<int> s = {x, y};
+        for (int i: cur)
+        {
+            int qwer = ask(i, x);
+            if (qwer % temp == 0)
+                s.insert(i);
+            if (qwer == n - 1)
+                return ask(x, i, true);
+        }
+        cur = vector<int>(all(s));
+        t = temp;
+        //cerr << "XY " << x << " " << y << endl;
+        //for (int i: cur)
+            //cerr << i << " ";
+        //cerr << endl;
+    }
+    return ask(cur[0], cur[1], true);
+}
+
+//{{{
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    ints(t); fori(t) if (run() == -1) exit(1);
+    cerr << fixed << setprecision(0) << "Execution time = " << 1000.0 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
+
+#define a _a
+#define n _n
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a);
+template<typename T,typename D=decltype(*begin(declval<T>())),typename enable_if<!is_same<T,basic_string<char>>::value>::type* =nullptr>
+ostream&operator<<(ostream&os,T const&a){auto it=begin(a);if(it!=end(a))os<<*it++;while(it!=end(a))os<<"\n "[is_fundamental<typename T::value_type>::value]<<*it++;return os;}
+ttt12i ostream&operator<<(ostream&os,pair<T1,T2>const&a){return os<<a.first<<" "<<a.second;}
+ttt12i istream&operator>>(istream&is,pair<T1,T2>&a){return is>>a.first>>a.second;}
+ttti   istream&operator>>(istream&is,vector<T>&a){fori(a.size())is>>a[i];return is;}
+ttti void print(T&&a){cout<<" "<<a;}
+ttti void priws(T&&a){cout<<a;}
+ttti void read(T&a){cin>>a;} //}}}
