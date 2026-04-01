3330657fc0c869e8bc75ed8dae2255f3d5119e88
igorjan94 <igorjan94@mail.ru>
2026-04-01 19:59:13 +0300

    2214 aprils fool

---
+++ CodeForce/2214/D.py
@@ -0,0 +1,17 @@
+n = int(input())
+if n == 1:
+    print('walk')
+elif n == 2:
+    print('no')
+elif n == 3:
+    print('no')
+elif n == 4:
+    print('no')
+elif n == 5:
+    print('yes')
+elif n == 6:
+    print('yes')
+elif n == 7:
+    print('backwards')
+else:
+    print(4)
